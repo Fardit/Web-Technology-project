03d440b43de360037aebe294466b916b0012e099
Fardit Ahamed Shawkot <39610210+Fardit@users.noreply.github.com>
2020-12-06 11:02:22 +0600

    Delete header.php

--- Final/lab task 1/PMS/includes/header.php
+++
@@ -1,5 +0,0 @@
-<div align="center" style = "width:100%;height:50px;background-color:black;">
-
-<a href="../index.php"><button style="width:20%;height:100%; background-color:blue; ">Home</button> </a>
-
-</div>
